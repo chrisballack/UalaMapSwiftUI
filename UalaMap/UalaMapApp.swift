53510282983d46f647d071c324433ba62aeb4d59
chrisballack <chris04140607077@gmail.com>
2024-11-01 14:50:45 -0500

    Project init, hello, world

---
+++ UalaMap/UalaMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaMapApp.swift
+//  UalaMap
+//
+//  Created by Maria Fernanda Paz Rodriguez on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UalaMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UalaMapTests/UalaMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  UalaMapTests.swift
+//  UalaMapTests
+//
+//  Created by Maria Fernanda Paz Rodriguez on 1/11/24.
+//
+
+import Testing
+@testable import UalaMap
+
+struct UalaMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
